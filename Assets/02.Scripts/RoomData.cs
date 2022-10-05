9929933383b219a0660e2d6fdd2d37baf67c2311
Lee JaeHyun <myleje@gmail.com>
2022-10-05 14:50:28 +0900

    .

--- Assets/02.Scripts/RoomData.cs
+++ Assets/02.Scripts/RoomData.cs
@@ -9,6 +9,28 @@ public class RoomData : MonoBehaviour
 {
     public TMP_Text roomText;
 
-    private
+    private RoomInfo roomInfo;
+
+    // 프로퍼티로 선언
+    public RoomInfo RoomInfo
+    {
+        get
+        {
+            return roomInfo;
+        }
+        set
+        {
+            roomInfo = value;
+
+            //룸 정보를 갱신   "MyRoom (5/20)"
+            string msg = $"{roomInfo.Name} ({roomInfo.PlayerCount}/{roomInfo.MaxPlayers})";
+            roomText.text = msg;
+        }
+    }
 
 }
+/*
+    var info = RoomData.RoomInfo; //get
+
+    RoomData.RoomInfo = {값}; //set
+*/
